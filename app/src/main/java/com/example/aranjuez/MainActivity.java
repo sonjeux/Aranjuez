cd8309dfcd178c89a6bac53536340dbbdd25f6f7
sonjeux <sonjeux@users.noreply.github.com>
2019-05-17 13:12:08 -0400

    Texto

--- app/src/main/java/com/example/aranjuez/MainActivity.java
+++ app/src/main/java/com/example/aranjuez/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aranjuez;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,9 +80,17 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.configuracion:
+                intent=new Intent(getApplicationContext(), configuracion.class);
+                startActivity(intent);
+                break;
+        }
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            intent=new Intent(getApplicationContext(), configuracion.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
--- app/src/main/java/com/example/aranjuez/configuracion.java
+++ app/src/main/java/com/example/aranjuez/configuracion.java
@@ -1,5 +1,4 @@
 package com.example.aranjuez;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
